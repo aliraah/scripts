3f355f91e290f8ba4ce599f6fea7f5f90f051798
Alireza Rahimzadeh <148966899+aliraah@users.noreply.github.com>
2024-04-17 19:35:25 +0200

    Update update_col.py

--- update_col.py
+++ update_col.py
@@ -1,40 +1,71 @@
-import pymongo
+from pymongo import MongoClient
+import subprocess
+import os
 import sys
-from pymongo.errors import DuplicateKeyError
+import json
 
-subdomains_textfile = sys.argv[1]
-organisation = sys.argv[2] 
+org = sys.argv[1]
 
-if len(sys.argv) >= 4:
-    found_by = sys.argv[3] 
-else:
-    found_by = "subfinder"
+client = MongoClient("<CONNECTION-STRING>")
+db = client.assets
+collection = db[f'{org}']
 
-# Function to read subdomains from file and insert into MongoDB
-def insert_subdomains(filename):
-    # Connect to MongoDB
-    client = pymongo.MongoClient("<CONNECTION-STRING>")
-    db = client["assets"]
-    collection = db[f"{organisation}"]
 
-    # Create a unique index on the "subdomain" field
-    collection.create_index("subdomain", unique=True)
+def query_subdomains():  
+    documents = collection.find()
     
-    # Open file and read subdomains
+    with open("subdomains.txt", "w") as file:
+        for document in documents:
+            subdomain = document.get('subdomain')
+            if subdomain:
+                file.write(subdomain + "\n")
+    
+    run_dnsx("subdomains.txt")
+
+def run_dnsx(filename):
+    try:
+        result = subprocess.run(['dnsx', '-silent', '-l', filename, '-o', 'dnsx.txt'], capture_output=True, text=True, check=True)
+        
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        return []
+
+    run_httpx("dnsx.txt")
+
+def run_httpx(filename):
+    try:
+        result = subprocess.run(['httpx', '-l', filename, '-json', '-o', 'httpx.txt'], capture_output=True, text=True, check=True)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        return []
+
+    update_documents("httpx.txt")
+
+
+def update_documents(filename):
+    # Initialize count for updated documents
+    updated_count = 0
+
+    # Open file and read information
     with open(filename, 'r') as file:
-        subdomains = file.readlines()
-        num_subdomains = 0 
-        # Insert each subdomain into MongoDB
-        for subdomain in subdomains:
-            subdomain = subdomain.strip()  # Remove leading/trailing whitespace
-            doc = {"subdomain": subdomain, "org": organisation, "found_by": found_by, "x_status_code": ""}
-            try:
-                collection.insert_one(doc)
-                num_subdomains += 1
-            except DuplicateKeyError:
-                print(f"Skipping duplicate subdomain: {subdomain}")
-                continue
+        for line in file:
+            data = json.loads(line)
+            input_key = data.get("input")
 
-    print(f"{num_subdomains} subdomains inserted into {organisation} collection.")
+            existing_doc = collection.find_one({"subdomain": input_key})
+            x_status_code = existing_doc.get("status_code")
+            data['x_status_code'] = x_status_code
 
-insert_subdomains(subdomains_textfile)
+            if input_key:
+                # Find document in MongoDB collection with matching input key and update it
+                result = collection.update_one({"subdomain": input_key}, {"$set": data}, upsert=True)
+                updated_count += result.modified_count
+
+    print(f"{updated_count} documents updated in {org}.")
+
+
+query_subdomains()
+os.remove("subdomains.txt")
+os.remove("dnsx.txt")
+os.remove("httpx.txt")
